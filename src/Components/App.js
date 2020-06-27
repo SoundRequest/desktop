6e42e096a710d26b10bbb1146fe3e3f5f2b04767
GangjunLee <me@gangjun.dev>
2020-06-27 20:25:03 +0900

    update signin ui

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,13 +6,13 @@ class App extends Component {
   render() {
     if (this.props.location.pathname == '/signin') {
       return (
-        <div id='App' className='bg-gray-10 0 text-gray-700'>
+        <div id='App' className='bg-gray-100 text-gray-700'>
           {this.props.children}
         </div>
       )
     }
     return (
-      <div id='App' className='bg-gray-10 0 text-gray-700'>
+      <div id='App' className='bg-gray-100 text-gray-700'>
         <SideNav />
         <div style={{ marginLeft: '240px' }}>{this.props.children}</div>
       </div>
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -9,7 +9,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='flex h-full'>
-        <div className='w-1/2 h-full' style={{ position: 'relative' }}>
+        <div className='w-1/2 h-full bg-white shadow-sm' style={{ position: 'relative' }}>
           <div
             className=''
             style={{
@@ -25,7 +25,9 @@ class SignIn extends React.Component {
           </div>
         </div>
         <form className='w-1/2 px-16'>
-          <div className='text-3xl font-bold mt-40'>SoundRequest</div>
+          <div className='text-3xl font-bold mt-40 text-center'>
+            SoundRequest
+          </div>
           <div className='md:flex md:items-center my-6 '>
             <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
@@ -33,19 +35,24 @@ class SignIn extends React.Component {
               placeholder='이메일'
             />
           </div>
-          <div className='md:flex md:items-center mb-6'>
+          <div className='md:flex md:items-center mb-4'>
             <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
               type='password'
               placeholder='비밀번호'
             />
           </div>
+          <div className='mb-6 text-center'>
+            <a className='text-md text-gray-600 hover:text-gray-800 cursor-pointer border-b-2 border-dashed'>
+              비밀번호 찾기
+            </a>
+          </div>
           <div className='md:flex md:items-center'>
-            <div className='md:w-2/3'>
+            <div className='mx-auto'>
               <button
-                className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-4 px-6 rounded'
+                className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-4 px-24 rounded-full'
                 type='button'>
-                    로그인
+                로그인
               </button>
             </div>
           </div>
