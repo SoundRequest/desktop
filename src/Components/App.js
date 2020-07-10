903983b8632249629d9320a173664c9c4452d53f
Nitro <nitro0@naver.com>
2020-07-11 01:52:24 +0900

    The player method was updated. You may have to install new dependencies.

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,8 +3,30 @@ import { withRouter } from 'react-router-dom'
 import SideNav from './SideNav'
 import PlayerBar from './Player/PlayerBar'
 import Lyrics from './Player/Lyrics.js'
+import ReactPlayer from 'react-player/youtube'
 
 class App extends Component {
+  //https://openbase.io/js/react-player
+  constructor() {
+    super();
+    this.state = {
+      url:null,
+      playing:false,
+      loop:false,
+      volume:0.5, //0~1
+    };
+  }
+  
+  updatePlayerStatus = (playerData) => {
+    this.setState({
+      url:playerData.url,
+      playing:playerData.playing,
+      loop:playerData.loop,
+      volume:playerData.volume,
+      pip:playerData.pip
+    });
+  }
+
   render() {
     /*
     if (this.props.location.pathname === '/signin') {
@@ -16,7 +38,8 @@ class App extends Component {
     }*/
     return (
       <div id='App' className='bg-gray-10 0 text-gray-700'>
-        <PlayerBar />
+        <ReactPlayer class='row' width='300px' height='300px' wed url={this.state.url} playing={this.state.playing} loop={this.state.loop} />
+        <PlayerBar updatePlayerStatus={this.updatePlayerStatus} />
         <SideNav />
         <Lyrics />
         <div style={{ marginLeft: '240px' }}>{this.props.children}</div>
--- src/Components/Player/PlayerBar.js
+++ src/Components/Player/PlayerBar.js
@@ -3,7 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faColumns } from '@fortawesome/free-solid-svg-icons'
 import { Link, withRouter } from 'react-router-dom'
 
-class PlayerBar extends Component {
+class PlayerBar extends React.Component {
+  playerData = {
+    url:'https://www.youtube.com/watch?v=_LLCz1FCWrY',
+    playing:false,
+    loop:false,
+    volume:0.5, //0~1
+  }
+
   componentDidMount() {
     var slider = document.getElementById("player");
     var output = document.getElementById("currentValue");
@@ -21,6 +28,11 @@ class PlayerBar extends Component {
         slider.style.background = color;
     })
   }
+
+  onPlayerStatusChange =()=> {
+    this.props.updatePlayerStatus(this.playerData);
+  }
+
   render() {
     return (
       <div id='playerBar'>
@@ -31,6 +43,8 @@ class PlayerBar extends Component {
           </div>
           <div class='col'><span id='targetValue'/></div>
         </div>
+        <button onClick={this.onPlayerStatusChange}>Setup Player</button>
+        <button onClick={() => {this.playerData.playing = !this.playerData.playing; this.onPlayerStatusChange();}}>재생/일시중지</button>
       </div>
     )
   }
